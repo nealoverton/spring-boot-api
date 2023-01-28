51dcb8279bc4770e1e53701b3193b356b6be8b94
nealoverton <nealjoverton@gmail.com>
2023-01-28 20:12:00 +0000

    create store table with seeded data

--- src/main/java/com/example/springbootapi/dbSeed/dbSeed.java
+++ src/main/java/com/example/springbootapi/dbSeed/dbSeed.java
@@ -1,5 +1,9 @@
-package com.example.springbootapi.item;
+package com.example.springbootapi.dbSeed;
 
+import com.example.springbootapi.item.Item;
+import com.example.springbootapi.item.ItemRepository;
+import com.example.springbootapi.store.Store;
+import com.example.springbootapi.store.StoreRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +11,10 @@ import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
-public class ItemConfig {
+public class dbSeed {
 
     @Bean
-    CommandLineRunner commandLineRunner(ItemRepository itemRepository) {
+    CommandLineRunner commandLineRunner(ItemRepository itemRepository, StoreRepository storeRepository) {
         return args -> {
             Item laptopStandSilver = new Item("Laptop Stand", "silver", 39.99, 3);
             Item laptopStandBlack = new Item("Laptop Stand", "black", 39.99, 1);
@@ -19,6 +23,13 @@ public class ItemConfig {
 
             itemRepository.saveAll(
                     List.of(laptopStandSilver, laptopStandBlack, wirelessMouseBlack, wirelessMouseWhite)
+            );
+
+            Store manchester = new Store("Manchester", "55-57 Bridge St, Manchester M3 3BQ");
+            Store hull = new Store("Hull", "1-73 Humber St, Hull HU1 1TU");
+
+            storeRepository.saveAll(
+                    List.of(manchester, hull)
             );
         };
 
---
+++ src/main/java/com/example/springbootapi/store/Store.java
@@ -0,0 +1,68 @@
+package com.example.springbootapi.store;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Store {
+    @Id
+    @SequenceGenerator(
+            name = "store_sequence",
+            sequenceName = "store_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "store_sequence"
+    )
+    private Long id;
+    private String name;
+    private String address;
+
+    public Store() {
+    }
+
+    public Store(Long id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Store(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Store{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
